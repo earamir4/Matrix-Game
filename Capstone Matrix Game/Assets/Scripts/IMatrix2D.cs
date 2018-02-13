08d83f29320da3e4dbf8d74cd71f647cb28e939f
Erick Ramirez Cordero <rami7535@gmail.com>
2018-02-12 17:25:39 -0700

    Added Matrix Interface and edited gitignore
    The gitignore was moved in the previous commit to another folder.
    Additionally, the gitignore also ignores the Visual Studio directory.

---
+++ Capstone Matrix Game/Assets/Scripts/IMatrix2D.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// <see cref="IMatrix2D"/> is an interface that defines the properies and functions all matrices
+/// should contain.
+/// </summary>
+public interface IMatrix2D
+{
+    int A { get; set; }
+    int B { get; set; }
+    int C { get; set; }
+    int D { get; set; }
+
+    IMatrix2D Multiply();
+}
