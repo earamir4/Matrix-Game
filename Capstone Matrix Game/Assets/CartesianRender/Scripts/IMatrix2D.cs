3e67e5c58fcff015001bd1f02cebaa9a97470341
Erick Ramirez Cordero <rami7535@gmail.com>
2018-02-13 17:32:04 -0700

    Added Matrix Interface
    - Added Scripts folder
    - Added IMatrix2D Interface

---
+++ Capstone Matrix Game/Assets/CartesianRender/Scripts/IMatrix2D.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// <see cref="IMatrix2D"/> is an interface which denotes values and functions that all matrices
+/// should implement.
+/// </summary>
+public interface IMatrix2D
+{
+    int A { get; set; }
+    int B { get; set; }
+    int C { get; set; }
+    int D { get; set; }
+
+    IMatrix2D Multiply();
+}
