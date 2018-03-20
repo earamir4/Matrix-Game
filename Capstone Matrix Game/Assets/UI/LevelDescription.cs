42168de44e79cf0706c2526662183e60d7a2e06b
Kyle Chapman <xeuorux@gmail.com>
2018-03-20 00:54:33 -0700

    Animation seek, level description area with button to toggle hints

---
+++ Capstone Matrix Game/Assets/UI/LevelDescription.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelDescription : MonoBehaviour
+{
+	public GameObject descriptionText;
+	public GameObject hintsText;
+	public Text buttonText;
+
+	public void SwapStuff()
+	{
+		if (descriptionText.activeSelf)
+		{
+			descriptionText.SetActive(false);
+			hintsText.SetActive(true);
+			buttonText.text = "Back To Description";
+        }
+		else
+		{
+			descriptionText.SetActive(true);
+			hintsText.SetActive(false);
+			buttonText.text = "Hints";
+		}
+	}
+}
