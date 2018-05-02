5faea7a936395af3ca45963c01af46681b61e056
deathbyhuskie <35600412+deathbyhuskie@users.noreply.github.com>
2018-05-02 05:33:44 -0700

    Update GSFU_Runtime.cs

--- Capstone Matrix Game/Assets/GSFU/GSFU_Runtime.cs
+++ Capstone Matrix Game/Assets/GSFU/GSFU_Runtime.cs
@@ -1,34 +1,63 @@
 ﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
 
 public class GSFU_Runtime : MonoBehaviour
 {
     public string playername;
+    public string url;
+    public string password;
+    public string id;
+    /*
+    private void Update()
+    {
+        playername = PlayerPrefs.GetString("Playername");
+        url = PlayerPrefs.GetString("URL");
+        password = PlayerPrefs.GetString("Password");
+        id = PlayerPrefs.GetString("ID");
 
-	void OnEnable()
-	{
-		// Suscribe for catching cloud responses.
-		CloudConnectorCore.processedResponseCallback.AddListener(GSFU_Demo_Utils.ParseData);
-	}
-	
-	void OnDisable()
-	{
-		// Remove listeners.
-		CloudConnectorCore.processedResponseCallback.RemoveListener(GSFU_Demo_Utils.ParseData);
-	}
-	
+    }
+    */
+    void OnEnable()
+    {
+        // Suscribe for catching cloud responses.
+        CloudConnectorCore.processedResponseCallback.AddListener(GSFU_Demo_Utils.ParseData);
+    }
+
+    void OnDisable()
+    {
+        // Remove listeners.
+        CloudConnectorCore.processedResponseCallback.RemoveListener(GSFU_Demo_Utils.ParseData);
+    }
+
     public void NameInput(string inputName)
     {
         playername = inputName;
+        PlayerPrefs.SetString("Name", playername);
+
     }
 
-    public void ChangeName()
+    public void URLInput(string inputURL)
+    {
+        url = inputURL;
+        PlayerPrefs.SetString("URL", url);
+    }
+
+    public void PWInput(string inputPW)
+    {
+        password = inputPW;
+        PlayerPrefs.SetString("Password", url);
+    }
+
+    public void IDInput(string inputID)
+    {
+        id = inputID;
+        PlayerPrefs.SetString("ID", url);
+    }
+
+    public void SubmitInfo(string inputName, string password, string URL, string ID)
     {
         if (playername != null)
-            PlayerPrefs.SetString("Playername", playername);
+        {
+            GSFU_Demo_Utils.createPlayer(playername);
+        }
     }
 }
-
-
-
